5d62de358bef8a1c70f5122527c5032130bf64d7
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2025-04-07 18:33:27 -0600

    updated days_on_hand to properly set as INT64 instead of string

--- processors.py
+++ processors.py
@@ -2,23 +2,31 @@ import polars as pl
 from constants import TOTAL_UNITS, UNITS_SOLD_LAST_30_DAYS, DAYS_ON_HAND,DEFAULT_DAYS_ON_HAND, INVALID_DAYS_ON_HAND
 
 def calculate_days_on_hand(*args, **kwargs):
+    total_units_col = (
+        pl.col(TOTAL_UNITS)
+        .cast(pl.Int64, strict=False)
+    )
+    units_sold_last_30_days_col = (
+        pl.col(UNITS_SOLD_LAST_30_DAYS)
+        .cast(pl.Int64, strict=False)
+    )
     return (
         pl.when(
-            (pl.col(TOTAL_UNITS) == 0) & (pl.col(UNITS_SOLD_LAST_30_DAYS) == 0)
+            (total_units_col == 0) & (units_sold_last_30_days_col == 0)
         )
         .then(pl.lit(DEFAULT_DAYS_ON_HAND))
         .when(
-            (pl.col(TOTAL_UNITS) == 0) | (pl.col(UNITS_SOLD_LAST_30_DAYS) == 0)
+            (total_units_col == 0) | (total_units_col == 0)
         )
         .then(pl.lit(INVALID_DAYS_ON_HAND))
         .when(
-            (pl.col(TOTAL_UNITS) >= 0) & (pl.col(UNITS_SOLD_LAST_30_DAYS) == 0)
+            (total_units_col >= 0) & (total_units_col == 0)
         )
-        .then(pl.col(TOTAL_UNITS))
+        .then(total_units_col)
         .otherwise(
-            ((pl.col(TOTAL_UNITS) / pl.col(UNITS_SOLD_LAST_30_DAYS)) * 30)
+            ((total_units_col / total_units_col) * 30)
             .round(2)
-            .cast(pl.Utf8)
+            .cast(pl.Int64, strict=False)
         )
         .alias(DAYS_ON_HAND)
     )
